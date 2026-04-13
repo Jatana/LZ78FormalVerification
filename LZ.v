1c69d44e7c54b1f6e81aafa2098c843ab987206c
Unstrois <unkiwi666@gmail.com>
2026-04-13 11:33:31 +0100

    Decompress implementation + example tests

--- LZ.v
+++ LZ.v
@@ -1,5 +1,5 @@
 From Stdlib Require Import Arith Strings.Byte List Lia.
-Require Import Utils LZ_Matching LZ_Tokens.
+Require Import Utils LZ_Matching LZ_Tokens LZ_Expand.
 Import ListNotations.
 
 Module Impl.
@@ -19,8 +19,16 @@ Module Impl.
   Definition compress (s: list byte): list Token :=
     compress' [] s 0.
 
-  Fixpoint decompress (l : list Token) : list byte := nil. 
+  Fixpoint decompress' (acc : list byte) (l : list Token) : list byte :=
+    match l with
+    | [] => acc
+    | Lit b :: tl => decompress' (acc ++ [b]) tl
+    | Ref len off :: tl => decompress' (expand_ref acc len off) tl
+    end.
 
+  Definition decompress (l : list Token) : list byte :=
+    decompress' [] l.
+  
   Theorem correctness: forall s,
     decompress (compress s) = s.
   Proof.
@@ -32,3 +40,56 @@ Module Impl.
   Admitted.
 
 End Impl.
+
+Section Tests.
+  Import Impl.
+
+  Definition empty : list byte := [].
+  Definition literals : list byte := ["010"%byte; "011"%byte].
+  Definition compressed_literals : list Token := [Lit "010"%byte; Lit "011"%byte].
+  Definition repeating : list byte := [zero; one; one; zero; one; zero; one; one; zero].
+  Definition overlapping : list byte := [zero; one; zero; one; zero; one].
+  Definition compressed_repeating : list Token := [Lit zero; Lit one; Lit one; Lit zero; Lit one; Ref 4 5].
+  Definition compressed_overlapping : list Token := [Lit zero; Lit one; Ref 4 2].
+  
+  Example compress_empty_test :
+    compress empty = [].
+  Proof. reflexivity. Qed.
+  
+  Example decompress_empty_test :
+    decompress [] = empty.
+  Proof. reflexivity. Qed.
+  
+  Example compress_literals_test :
+    compress literals = compressed_literals.
+  Proof. reflexivity. Qed.
+  
+  Example decompress_literals_test :
+    decompress compressed_literals = literals.
+  Proof. reflexivity. Qed.
+  
+  Example compress_repetition_test :
+    compress repeating = compressed_repeating.
+  Proof. reflexivity. Qed.
+  
+  Example decompress_overlap_test :
+    decompress compressed_overlapping = overlapping.
+  Proof. reflexivity. Qed.
+  
+  Example correctness_empty :
+    decompress (compress empty) = empty.
+  Proof. reflexivity. Qed.
+  
+  Example correctness_simple :
+    decompress (compress [zero]) = [zero].
+  Proof. reflexivity. Qed.
+  
+  Example correctness_repeating :
+    decompress (compress repeating) = repeating.
+  Proof. reflexivity. Qed.
+  
+  Example correctness_overlapping :
+    decompress (compress overlapping) = overlapping.
+  Proof. reflexivity. Qed.
+
+End Tests.
---
+++ LZ_Expand.v
@@ -0,0 +1,99 @@
+From Stdlib Require Import Arith Strings.Byte List Lia.
+Require Import Utils LZ_Tokens.
+Import ListNotations.
+
+Module Expand.
+
+  Definition nth_byte (l : list byte) (n : nat) : byte :=
+    nth n l x00.
+
+  Fixpoint expand_ref (acc : list byte) (len off : nat) : list byte :=
+    match len with
+    | 0 => acc
+    | S len' =>
+        let b := nth_byte acc (length acc - off) in
+        expand_ref (acc ++ [b]) len' off
+    end.
+
+  Lemma nth_byte_eq_nth :
+    forall l n,
+      nth_byte l n = nth n l x00.
+  Proof.
+    reflexivity.
+  Qed.
+
+  Lemma expand_ref_0 :
+    forall acc off,
+      expand_ref acc 0 off = acc.
+  Proof.
+    reflexivity.
+  Qed.
+
+  Lemma expand_ref_S :
+    forall acc len off,
+      expand_ref acc (S len) off =
+        expand_ref (acc ++ [nth_byte acc (length acc - off)]) len off.
+  Proof.
+    reflexivity.
+  Qed.
+
+  Lemma expand_ref_length :
+    forall acc len off,
+      length (expand_ref acc len off) = length acc + len.
+  Proof.
+    intros acc len. revert acc.
+    induction len; intros.
+    - simpl. lia.
+    - simpl. rewrite IHlen.
+      rewrite length_app.
+      simpl. lia.
+  Qed.
+
+  Lemma expand_ref_preserves_prefix_length :
+    forall acc len off,
+      off <= length acc ->
+      length acc <= length (expand_ref acc len off).
+  Proof.
+    intros.
+    rewrite expand_ref_length.
+    lia.
+  Qed.
+
+End Expand.
+Export Expand.
+
+Section Tests.
+  Definition zero := "0"%byte.
+  Definition one := "1"%byte.
+    
+  Example nth_byte_test0 :
+    nth_byte [zero; one] 0 = zero.
+  Proof. reflexivity. Qed.
+
+  Example nth_byte_test1 :
+    nth_byte [zero; one] 1 = one.
+  Proof. reflexivity. Qed.
+
+  Example nth_byte_default_test :
+    nth_byte [zero; one] 5 = x00.
+  Proof. reflexivity. Qed.
+
+  Example expand_ref_len0_test :
+    expand_ref [zero; one] 0 1 = [zero; one].
+  Proof. reflexivity. Qed.
+
+  Example expand_ref_one_byte_test :
+    expand_ref [zero; one] 1 1 = [zero; one; one].
+  Proof. reflexivity. Qed.
+
+  Example expand_ref_nonoverlap_test :
+    expand_ref [zero; one; one; zero; one] 4 5 =
+      [zero; one; one; zero; one; zero; one; one; zero].
+  Proof. reflexivity. Qed.
+
+  Example expand_ref_overlap_test :
+    expand_ref [zero; one] 4 2 =
+      [zero; one; zero; one; zero; one].
+  Proof. reflexivity. Qed.
+
+End Tests.
